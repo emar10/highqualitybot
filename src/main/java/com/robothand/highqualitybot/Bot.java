7177feb09d4ae3d2d0ed4f31d1aeb9bbca6f69b1
sasquench <docheckler@gmail.com>
2017-06-09 13:33:58 -0400

    Initial commit

---
+++ src/main/java/com/robothand/highqualitybot/Bot.java
@@ -0,0 +1,12 @@
+package com.robothand.highqualitybot;
+
+/**
+ * main class
+ */
+public class Bot {
+
+    public static void main(String[] args) {
+        System.out.println("it probably works");
+    }
+
+}
