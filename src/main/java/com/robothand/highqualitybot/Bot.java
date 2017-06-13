7cde817ef923f9a94825ccf9cb7e1fe63a0dfba3
sasquench <docheckler@gmail.com>
2017-06-12 20:37:50 -0400

    remove API key from source code, because that's just poor form (old one is invalid, by the way)

--- src/main/java/com/robothand/highqualitybot/Bot.java
+++ src/main/java/com/robothand/highqualitybot/Bot.java
@@ -4,6 +4,10 @@ import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * main class
  */
@@ -12,9 +16,27 @@ public class Bot {
     static boolean running;
 
     public static void main(String[] args) {
+        File config;
+        String token;
+
+        System.out.println("Attempting to read \"config.txt\"");
+        config = new File("config.txt");
+        token = null;
+
+        try {
+            Scanner s = new Scanner(config);
+
+            // token
+            token = s.nextLine();
+
+        } catch (FileNotFoundException e) {
+            System.err.println("FATAL: could not find file \"config.txt\"");
+            System.exit(1);
+        }
+
         System.out.println("Connecting to Discord...");
         try {
-            api = new JDABuilder(AccountType.BOT).setToken("MjY4MjExODI2MzU1ODYzNTUz.DByjTQ.1ycw5crEGVZGo-ckyGakydsno0o").buildAsync();
+            api = new JDABuilder(AccountType.BOT).setToken(token).buildAsync();
         } catch (Exception e) {
             System.err.println("FATAL: Could not connect to Discord");
             System.exit(1);
