8fc86ab33f03744633ac124e4edaa64fc3f73548
sasquench <docheckler@gmail.com>
2017-06-19 09:31:21 -0400

    implement TrackScheduler

--- src/main/java/com/robothand/highqualitybot/music/TrackScheduler.java
+++ src/main/java/com/robothand/highqualitybot/music/TrackScheduler.java
@@ -16,9 +16,10 @@ public class TrackScheduler extends AudioEventAdapter {
         OFF, SINGLE, ALL
     }
 
-    Repeat repeating;
-    final Queue<AudioTrack> queue;
-    final AudioPlayer player;
+    private Repeat repeating;
+    private final Queue<AudioTrack> queue;
+    private final AudioPlayer player;
+    private AudioTrack prevTrack;
 
     public TrackScheduler(AudioPlayer player) {
         this.player = player;
@@ -27,16 +28,35 @@ public class TrackScheduler extends AudioEventAdapter {
 
     // TODO implement scheduler methods
     public void add(AudioTrack track) {
-
+        if (!player.startTrack(track, true)) {
+            queue.offer(track);
+        }
     }
 
     public void nextTrack() {
+        nextTrack(false);
+    }
 
+    public void nextTrack(boolean forceNext) {
+        if (repeating == Repeat.OFF || forceNext) {
+            player.startTrack(queue.poll(), false);
+        } else if (repeating == Repeat.SINGLE) {
+            player.startTrack(prevTrack.makeClone(), false);
+        } else if (repeating == Repeat.ALL) {
+            if (prevTrack == null) {
+                prevTrack = player.getPlayingTrack();
+            }
+
+            player.startTrack(queue.poll(), false);
+            queue.add(prevTrack);
+            prevTrack = null;
+        }
     }
 
     @Override
     public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason reason) {
-
+        prevTrack = track;
+        nextTrack();
     }
 
     public Repeat getRepeat() {
