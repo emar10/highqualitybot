ffc03f96b60b279dd1f7ea8a195990accc3bd3db
sasquench <docheckler@gmail.com>
2017-06-27 18:35:26 -0400

    implement logic for clear command

--- src/main/java/com/robothand/highqualitybot/command/ClearCommand.java
+++ src/main/java/com/robothand/highqualitybot/command/ClearCommand.java
@@ -1,7 +1,13 @@
 package com.robothand.highqualitybot.command;
 
 import com.robothand.highqualitybot.Bot;
+import com.robothand.highqualitybot.music.GuildMusicPlayer;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+import java.util.Queue;
 
 /**
  * Created by ethan on 6/27/17.
@@ -26,6 +32,22 @@ public class ClearCommand extends Command {
 
     @Override
     public void onCommand(MessageReceivedEvent event, String[] args) {
+        GuildMusicPlayer musicPlayer;
+        Guild guild = event.getGuild();
+        MessageChannel channel = event.getChannel();
+        String message;
 
+        musicPlayer = GuildMusicPlayer.getPlayer(guild);
+
+        if (musicPlayer.getPlayingTrack() == null) {
+            message = "The player is already stopped!";
+        } else {
+            musicPlayer.getQueue().clear();
+            musicPlayer.skipTrack();
+
+            message = "The queue has been emptied and the player stopped!";
+        }
+
+        channel.sendMessage(message).queue();
     }
 }
