daf92a4be47c8dda42c5c73e31d48a7c169f22fd
Ethan Martin <ethan@ethanmartin.net>
2017-12-18 20:43:04 -0500

    bump version to v0.3.1

--- src/main/java/com/robothand/highqualitybot/Bot.java
+++ src/main/java/com/robothand/highqualitybot/Bot.java
@@ -18,7 +18,7 @@ import java.io.FileNotFoundException;
 public class Bot {
     private static JDA api;
     private static String CONFIGNAME = "config.cfg";
-    public static final String VERSION = "0.3";
+    public static final String VERSION = "0.3.1";
 
     public static void main(String[] args) {
         // get logger
